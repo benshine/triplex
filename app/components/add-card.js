5c4c88cfe04f740b017788feb48963850d411e83
Benji Shine <git@bshine.net>
2018-05-06 11:00:45 -0700

    ooooo

--- app/components/add-card.js
+++ app/components/add-card.js
@@ -3,6 +3,6 @@ import Component from '@ember/component';
 export default Component.extend({
   click(e) {
     console.log( 'click', e  );
-    this.get('onAddCard')('yay', this.get('group'));
+    this.get('onAddCard')();
   }
 });
--- app/components/status-group.js
+++ app/components/status-group.js
@@ -3,8 +3,10 @@ import Component from '@ember/component';
 export default Component.extend({
   tag: '',
   actions: {
-    addCard(name) {
+    addCard() {
+      const name = window.prompt('name for taks');
       console.log( 'add cand', name , this.get('group.name') );
+      this.get('group.tasks').addObject({name})
     }
   }
 });
