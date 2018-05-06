3dba3d2ce4369dd85f835a6482ebca459d1bd98f
Benji Shine <git@bshine.net>
2018-05-06 08:53:04 -0700

    hide edit button until hover

--- app/components/movie-poster.js
+++ app/components/movie-poster.js
@@ -1,15 +1,11 @@
 import Component from '@ember/component'
 
 export default Component.extend({
-  classNames: ['ma2', 'pa4', 'bg-moon-gray', 'br4', 'link', 'hover-bg-light-blue'],
+  classNames: ['ma2', 'pa4', 'bg-moon-gray', 'br4', 'link', 'hover-bg-light-blue', 'hide-child'],
   classNameBindings: ['isActive:bg-light-purple'],
   isEditing: false,
 
   actions: {
-
-    updateName (e) {
-      console.log({e})
-    },
     toggleEdit () {
       this.toggleProperty('isEditing');
     }
