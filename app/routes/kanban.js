d6d02600424b0cd9c2e43a96c0f2bbd629b70a8b
Benji Shine <git@bshine.net>
2018-05-06 10:25:31 -0700

    Rendering lists of status groups and columns without tasks

--- app/routes/kanban.js
+++ app/routes/kanban.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  groupNames: ['A', 'B', 'C', 'D'],
   buildTasks() {
     let tasks = [];
     for (let i = 0; i < 100; i++) {
@@ -10,7 +12,20 @@ export default Route.extend({
   },
 
   model() {
+    this.set('groups', this.get('groupNames').map(
+      (n) => {
+        return Ember.Object.create({
+          name: n,
+          tasks: []
+        });
+      }
+
+    ));
     this.set('tasks', this.buildTasks())
-    return this.get('tasks');
+    return {
+      tasks:   this.get('tasks'),
+      groups: this.get('groups')
+    }
+
   }
 });
