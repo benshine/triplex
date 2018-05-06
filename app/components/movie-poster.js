98797526e5b8ee8f3cfdec823fdac150179a55b2
Benji Shine <git@bshine.net>
2018-05-06 08:38:51 -0700

    not great way to edit movie name

--- app/components/movie-poster.js
+++ app/components/movie-poster.js
@@ -1,11 +1,17 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['ma2', 'pa4', 'bg-moon-gray', 'br4', 'link', 'dim', 'hover-bg-light-blue'],
+  classNames: ['ma2', 'pa4', 'bg-moon-gray', 'br4', 'link', 'hover-bg-light-blue'],
   classNameBindings: ['isActive:bg-light-purple'],
   isActive: false,
   click() {
     console.log( `click on ${this.get('movie.name')}`  );
     this.toggleProperty('isActive');
+  },
+  actions: {
+
+  updateName(e) {
+    console.log( {e}  );
+  }
   }
 });
